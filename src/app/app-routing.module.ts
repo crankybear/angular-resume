c8284a2ef3118b2fd280d1fe8491f48acab16ba7
half-pint-bear <half-pint-bear@debian-msi.lan>
2021-06-05 15:51:40 +0200

    set home as default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ContactComponent} from './contact/contact.component';
+import {FormationComponent} from './formation/formation.component';
+import {SoftComponent} from './soft/soft.component';
+import {ExpComponent} from './exp/exp.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'infos', component: ContactComponent},
+  {path: 'formations', component: FormationComponent},
+  {path: 'softskills', component: SoftComponent},
+  {path: 'exp', component: ExpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import { ContactComponent } from './contact/contact.component';
+import { ExpComponent } from './exp/exp.component';
+import { SoftComponent } from './soft/soft.component';
+import { FormationComponent } from './formation/formation.component';
+import { HomeComponent } from './home/home.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ExpComponent,
+    SoftComponent,
+    FormationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
